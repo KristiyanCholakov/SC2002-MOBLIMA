314a117c6953830e323e997ca14347f2b198d03e
Multijugador <denz0003@e.ntu.edu.sg>
2022-10-14 15:59:40 +0800

    :D

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Eldrick's d is huge!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to MOBLIMA");
+        System.out.println("are you a staff or a customer?");
+        System.out.println("1. Staff");
+        System.out.println("2. Customer");
+        int input = scan.nextInt();
+//        if (input == 1) {
+//            login();
+//        }
     }
 }
