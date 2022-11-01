6434c3a2f2084f6df700ab9ca045285dd176c95e
Xiaoyany88 <zhangxy692@gmail.com>
2022-11-01 17:15:28 +0800

    CINEPLEX WIP

--- src/pages/CineplexEditorPages.java
+++ src/pages/CineplexEditorPages.java
@@ -1,5 +1,13 @@
 package pages;
 
+import models.cinemas.Cinema;
+import models.cinemas.Cineplex;
+import models.cinemas.ShowTime;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class CineplexEditorPages {
@@ -9,12 +17,68 @@ public class CineplexEditorPages {
         while (running) {
             System.out.println("Select the action you want:\n" +
                     "(Type the number of the choice)\n" +
-                    "       1 - Add cinemas\n" +
-                    "       2 - Add locations\n" +
-                    "       3 - Add screens\n" +
-                    "       4 - Display cinema list\n" +
-                    "       5 - Back to Editor Portal");
+                    "       1 - Add Cineplex\n" +
+                    "       2 - Edit Cineplex\n" +
+                    "       3 - Delete Cineplex\n" +
+                    "       4 - Back to Editor Portal" );
+
+            System.out.print("Choice: ");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    addCineplexes();
+                    break;
+                case 2:
+                    editCinema();
+                    break;
+                case 3:
+                    deleteCinema();
+                    break;
+                case 4:
+                    running = false;
+                    break;
+                default:
+                    PageElements.printConsoleMessage("Invalid Choice!");
+            }
         }
+
+    }
+    public static void addCineplexes(){
+        ArrayList<Cinema> cinemas = null;
+        HashMap<LocalDate, ArrayList<ShowTime>> schedules = new HashMap<>();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Name of new cineplex: ");
+        String CineplexName = sc.nextLine();
+        // 3 cinemas under each cineplex
+        for (int i = 0; i < 3; i++){
+            System.out.println("Name of " + (i+1) +"st cinema under it: ");
+            String cinemaName = sc.nextLine();
+            String cinemaAddress = sc.nextLine();
+            while (true){
+                //add showtimes
+                System.out.println("Enter a date (like m/d/yyyy): ");
+                LocalDate newDate = dateInput(sc.nextLine());
+            }
+
+        }
+        //Cineplex cineplex = new Cineplex(cineplexName);
+
+    }
+    public static LocalDate dateInput(String userInput) {
+
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("M/d/yyyy");
+        LocalDate date = LocalDate.parse(userInput, dateFormat);
+
+
+        System.out.println(date);
+        return date ;
+    }
+
+    public static void editCinema(){
+
+    }
+    public static void deleteCinema(){
 
     }
 }
