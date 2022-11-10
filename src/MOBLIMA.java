a54e47d74d5bb968a6a96d49d36742d78803abda
Kristiyan Cholakov <krischolakov@icloud.com>
2022-11-11 02:37:28 +0800

    moblima javadoc

--- src/MOBLIMA.java
+++ src/MOBLIMA.java
@@ -2,7 +2,18 @@ import data_managers.PricesManager;
 import models.Prices;
 import pages.MainPage;
 
+/**
+ *  The MOBLIMA class starts the program.
+ *
+ * @author Kristiyan Cholakov (KrisCholakov02)
+ * @version 10/11/22
+ */
 public class MOBLIMA {
+
+    /**
+     * Starts the program and load the prices if none are detected.
+     * @param args
+     */
     public static void main(String[] args) {
         if (PricesManager.readPrices().size() == 0) {
             Prices prices = new Prices();
