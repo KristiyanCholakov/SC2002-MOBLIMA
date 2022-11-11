75ea511e4f32f76adab024e1983a7634f0dccb63
brendontan03 <brendontan003@gmail.com>
2022-11-11 12:36:09 +0800

    Updated formatting for exploreMovies();

--- src/pages/user/ExplorePages.java
+++ src/pages/user/ExplorePages.java
@@ -227,12 +227,13 @@ public class ExplorePages {
         System.out.println("MOVIES:\n");
         for (int i = 0; i < movies.size(); i++) {
             Movie movie = movies.get(i);
-            System.out.println(movie.getTitle());
-            System.out.println(movie.genresToString());
-            System.out.println(movie.getSynopsis());
-            System.out.println("Rating: " + movie.getRating() + ", Tickets Sold: " + movie.getTicketsSold());
-            System.out.println(movie.getStatus().getDescription());
-            System.out.println(movie.getType().getType()+"\n");
+            System.out.println("Movie " + (i+1) + ":");
+            System.out.println("Title: " + movie.getTitle());
+            System.out.println("Genre: " + movie.genresToString());
+            System.out.println("Synopsis: " + movie.getSynopsis());
+            System.out.println("Rating: " + movie.getRating() + ", Number of Tickets Sold: " + movie.getTicketsSold());
+            System.out.println("Status: " + movie.getStatus().getDescription());
+            System.out.println("Type: " + movie.getType().getType()+"\n");
         }
         Scanner scanner = new Scanner(System.in);
         boolean running = true;
